6ceed57bbdd6d5fd6ab870010a8b24552a5093a1
François Doussin <doussin.f@gmail.com>
2022-02-15 08:05:56 +0100

    refacto

--- src/App.js
+++ src/App.js
@@ -6,42 +6,61 @@ import './App.css';
 function App() {
   const planetContainer = useRef(null);
 
-  useEffect(() => {
-    const innerWidth = planetContainer.current.offsetWidth;
-    const innerHeight = planetContainer.current.offsetHeight;
+  const Scene = () => new THREE.Scene();
 
-    let scene = new THREE.Scene();
-    let camera = new THREE.PerspectiveCamera( 50, innerWidth/innerHeight, 0.1, 1000 );
-    let renderer = new THREE.WebGLRenderer();
+  const Camera = () => {
+    const camera = new THREE.PerspectiveCamera(
+      50,
+      planetContainer.current.offsetWidth/planetContainer.current.offsetHeight,
+      0.1,
+      1000
+    );
+    camera.position.z = 10;
+    return camera;
+  };
 
-    renderer.setSize( innerWidth, innerHeight );
+  const Renderer = () => {
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(
+      planetContainer.current.offsetWidth,
+      planetContainer.current.offsetHeight
+    );
+    return renderer;
+  };
 
-    planetContainer.current.appendChild( renderer.domElement );
-
-    let geometry = new THREE.SphereGeometry( 3, 64, 32 );
+  const Planet = () => {
+    const geometry = new THREE.SphereGeometry( 3, 64, 32 );
     const texture = new THREE.TextureLoader().load( moon );
-    let material = new THREE.MeshStandardMaterial( { map: texture } );
-    let cube = new THREE.Mesh( geometry, material );
+    const material = new THREE.MeshStandardMaterial( { map: texture } );
+    return new THREE.Mesh( geometry, material );
+  };
 
+  const Spotlight = () => {
     const spotLight = new THREE.SpotLight( 0xffffff );
     spotLight.position.set( 100, 100, -10 );
+    return spotLight;
+  };
+  
+  useEffect(() => {
+    const scene = Scene();
+    const camera = Camera();
+    const renderer = Renderer();
+    const planet = Planet();
 
-    scene.add( cube, spotLight );
-    camera.position.z = 10;
-    
+    planetContainer.current.appendChild( renderer.domElement );
+    scene.add( planet, Spotlight() );
+
     var animate = function () {
       requestAnimationFrame( animate );
-      cube.rotation.x += 0.002;
-      cube.rotation.y += 0.002;
+      planet.rotation.x += 0.001;
+      planet.rotation.y += 0.002;
       renderer.render( scene, camera );
     };
-    
+
     animate();
   }, []);
 
-  return (
-    <div id="planet" ref={planetContainer} />
-  );
+  return <div id="planet" ref={planetContainer} />;
 }
 
 export default App;
